f31737e9a60cc169deb9b183fecf54685246c527
Poghos <119891530+PoghosKeshishyan@users.noreply.github.com>
2024-03-06 15:52:00 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState, useEffect, useRef } from "react"
+
+export function App() {
+  const [data, setData] = useState([]);
+  const [search, setSearch] = useState('');
+  const [display, setDisplay] = useState(false);
+  const appRef = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("mousedown", handleClickOutSide);
+    
+    fetch('https://jsonplaceholder.typicode.com/photos?_limit=20')
+      .then(response => response.json())
+      .then(json => setData(json))
+  }, []);
+
+  const handleClickOutSide = (e) => {
+    if (!appRef.current.contains(e.target)) setDisplay(false);
+  }
+
+  const update = (title) => {
+    let str = title.split('').splice(0, 4).join('') + '...';
+    setSearch(str);
+    setDisplay(false);
+  }
+
+  return (
+    <div className="App" ref={appRef}>
+      <input
+        placeholder="Type to search"
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+        onClick={() => setDisplay(true)}
+      />
+      
+      <div className="Container">
+        {
+          display && data
+            .filter(el => el.title.includes(search))
+            .map((el) =>
+              <div className="item" key={el.id} onClick={() => update(el.title)}>
+                <span>{el.title}</span>
+                <img src={el.thumbnailUrl} alt="" width="50px" heigth="50px" />
+              </div>
+            )
+        }
+      </div>
+    </div>
+  )
+}
